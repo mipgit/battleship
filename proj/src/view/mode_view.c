19cbfbc5903a4fafc42774b47b505606d7ea0d28
Inês <up202306659@g.uporto.pt>
2025-05-30 19:45:57 +0100

    Changed mode sprite

--- proj/src/view/mode_view.c
+++ proj/src/view/mode_view.c
@@ -6,7 +6,7 @@ extern Sprite* multi_player;
 
 void draw_mode(uint8_t *buffer) {
   fill_screen(CREAM, buffer);
-  draw_sprite(choose_mode, 90, 80, buffer); 
+  draw_sprite(choose_mode, 60, 80, buffer); 
   draw_sprite(single_player, 85, 350, buffer);
   draw_sprite(multi_player, 80, 430, buffer);
 }
